13a030a4f2464180aa863ce3317be7c27658898f
Danila Bublik <danila.bublik@figured.com>
2022-01-29 22:26:24 +1300

    started the exercise

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+
+                    <template v-for="inv in inventory">
+                        {{inv.date}}
+                        {{inv.type}}
+                        {{inv.quantity}}
+                        {{inv.unitPrice}}<br/>
+                    </template>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                inventory: [
+                    {
+                        date: '05/06/2020',
+                        type: 'Purchase',
+                        quantity: 10,
+                        unitPrice: 5
+                    },
+                    {
+                        date: '07/06/2020',
+                        type: 'Purchase',
+                        quantity: 30,
+                        unitPrice: 4.5
+                    },
+                    {
+                        date: '08/06/2020',
+                        type: 'Application',
+                        quantity: -20,
+                    },
+                ]
+            }
+        }
+    }
+</script>
